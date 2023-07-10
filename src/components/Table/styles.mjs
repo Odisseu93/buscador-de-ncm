d7c7a7841e0432f2e673eba1a1e5969b5158fa87
Ulisses Silvério <odisseu93@localhost.localdomain>
2023-07-10 16:45:50 -0300

    feat: add Table component

---
+++ src/components/Table/styles.mjs
@@ -0,0 +1,12 @@
+export const Table = `
+  display: block;
+
+  width: clamp(calc(375px - 32px), 100%, 600px);
+  height: 75%;
+
+  padding: 1rem;
+  margin: 3rem auto; 
+
+  font-size: 1.25rem; 
+  overflow-y: scroll;
+`
