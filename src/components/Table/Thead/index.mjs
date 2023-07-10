1ae4031612496f36d1bbe90fa320b887382d7ba5
Ulisses Silvério <odisseu93@localhost.localdomain>
2023-07-10 16:47:57 -0300

    feat: add Thead component

---
+++ src/components/Table/Thead/index.mjs
@@ -0,0 +1,15 @@
+import { Th } from '../Th/index.mjs'
+import { Tr } from '../Tr/index.mjs'
+import * as S from './styles.mjs'
+
+
+export const Thead = ({ children = [] }) => {
+  const thTextcontentList = children
+
+  return `
+<thead style='${S.Thead.split('').join('')}'>
+${Tr({
+    children: thTextcontentList.map(thTextcontent => Th({ children: thTextcontent }))
+  })}
+</thead>
+`}
---
+++ src/components/Table/Thead/styles.mjs
@@ -0,0 +1,8 @@
+export const Thead = `background-color: #FFF;
+
+position: sticky; 
+top: -14px;
+
+width: 90%;
+
+padding: 0.25rem;`
