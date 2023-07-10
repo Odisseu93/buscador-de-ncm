f37282496fa06993e3a06ff9a63061173a5cd701
Ulisses Silvério <odisseu93@localhost.localdomain>
2023-07-10 15:16:21 -0300

    feat: add Footer component

---
+++ src/components/Footer/index.mjs
@@ -0,0 +1,11 @@
+import * as S from './styles.mjs'
+
+export const Footer = () => (`
+  <footer class="footer">
+    <style>${S.Footer}</style>
+    
+    <p>Desenvolvido Por 
+      <a href="http://github.com/odisseu93" target="_blank" rel="noopener noreferrer">Ulisses Silvério</a>
+    </p>
+  </footer>
+`)
---
+++ src/components/Footer/styles.mjs
@@ -0,0 +1,15 @@
+export const Footer =`
+.footer {
+  position: fixed;
+  bottom: 1rem;
+  margin-left: 1rem;
+}
+
+.footer a {
+  text-decoration: none;
+}
+
+.footer a:hover {
+  text-decoration: underline;
+}
+`
