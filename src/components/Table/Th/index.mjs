120f2bb66d07ae1817d6bf46b54ba24a6d118e79
Ulisses Silvério <odisseu93@localhost.localdomain>
2023-07-10 16:47:37 -0300

    feat: add Th component

---
+++ src/components/Table/Th/index.mjs
@@ -0,0 +1,7 @@
+import * as S from './styles.mjs'
+
+/**
+ @param children element html
+ */
+
+export const Th = ({ children }) =>  `<th style='${S.Th.split('').join('')}'>${children}</th>`
---
+++ src/components/Table/Th/styles.mjs
@@ -0,0 +1,7 @@
+export const Th  = `
+  width: 90%;
+
+  padding: 0.25rem;
+
+  outline: 2px solid #000;
+` 
