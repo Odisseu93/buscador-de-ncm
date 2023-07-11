e0bbba71f725d34cc92ac1b31b1b8ee4dceb7569
Ulisses Silvério <odisseu93@localhost.localdomain>
2023-07-11 16:06:17 -0300

    feat: add ApiRequestProblem componen

---
+++ src/components/ApiRequestProblem/index.mjs
@@ -0,0 +1,4 @@
+import * as S from './styles.mjs'
+
+export const ApiRequestProblem = () => ( `
+  <p style="${S.ApiRequestProblem.split('').join('')}">Houve algum problema &#9785;, tente Novamente mais tarde!</p>`)
---
+++ src/components/ApiRequestProblem/styles.mjs
@@ -0,0 +1,8 @@
+export const ApiRequestProblem  = `
+  display: block;
+  width: 100% ;
+
+  color: #EF6262;
+  font-weight: bold ;
+  text-align: center;
+` 
