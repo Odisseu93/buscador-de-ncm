76e4b1d3bd0b88154b2cb9b5e6f1dd547bc3f191
Ulisses Silvério <odisseu93@localhost.localdomain>
2023-07-11 11:02:17 -0300

    feat: add Title component

---
+++ src/components/Title/index.mjs
@@ -0,0 +1,6 @@
+import * as S from './styles.mjs'
+
+export const Title = () => ( `
+  <h1 style='${S.Title.split('').join('')}'>Buscador de NCM
+    <small style='${S.Small.split('').join('')}'>Nomenclatura Comum do Mercosul</small>
+  </h1>`)
---
+++ src/components/Title/styles.mjs
@@ -0,0 +1,11 @@
+export const Title  = `
+  display: flex;
+  flex-flow: column wrap;
+
+  margin-left: 1rem;
+  text-align: center;
+  font-size: 2.5rem;
+` 
+
+
+export const Small = `font-size: 1rem;`
