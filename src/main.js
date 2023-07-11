aebcec7ab3f2873c6d58d9b19e705a6933d81c20
Ulisses Silvério <odisseu93@localhost.localdomain>
2023-07-11 10:51:32 -0300

    feat: add main.js file and #root styles

--- src/main.js
+++ src/main.js
@@ -1,1 +1,16 @@
-console.log('Hello tech world!');
+import { App } from "./App.mjs"
+import { Footer } from "./components/Footer/index.mjs"
+import { WrapperHTML } from "./helpers/WrapperHTML.mjs"
+import { updateComponent } from "./helpers/updateComponent.mjs"
+import * as S from './styles/mainStyles.mjs'
+
+updateComponent(document.querySelector('#root'),
+  WrapperHTML(
+    {
+      HTML: [
+        `<style>${S.Main.split('').join('')}</style>`,
+        App(),
+        Footer(),
+      ]
+    })
+)
---
+++ src/styles/mainStyles.mjs
@@ -0,0 +1,7 @@
+export const Main = `
+  width: 100vw;
+  height: 100vh;
+`
+
+
+
