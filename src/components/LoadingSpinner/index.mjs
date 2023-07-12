882e974b971eed5f7005181ec6149dfc4b19eff0
Ulisses Silvério <odisseu93@localhost.localdomain>
2023-07-12 16:07:40 -0300

    feat: add LoadingSpinner component

---
+++ src/components/LoadingSpinner/index.mjs
@@ -0,0 +1,7 @@
+import * as S from './styles.mjs'
+
+export const LoadingSpinner = ()=> `    
+<div>
+  <div style="${S.loadingSpinner.split('').join('')}"> </div>
+</div>
+`
---
+++ src/components/LoadingSpinner/styles.mjs
@@ -0,0 +1,13 @@
+export const loadingSpinner =  `
+  display: block;
+
+  width: 50px;
+  height: 50px;
+
+  margin: 0.5rem auto;
+  border: 10px solid #f3f3f3;
+  border-top: 10px solid #383636;
+  border-radius: 50%;
+
+  animation: spinner 1.5s linear infinite;
+`
