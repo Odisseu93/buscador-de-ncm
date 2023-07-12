6ae6aa75b1ec3260c147ec9e8a141a7eb808b10d
Ulisses Silvério <odisseu93@localhost.localdomain>
2023-07-12 16:38:37 -0300

    fix: adjust Table styles

--- src/components/Table/styles.mjs
+++ src/components/Table/styles.mjs
@@ -1,12 +1,13 @@
 export const Table = `
+  box-sizing: border-box;
   display: block;
 
-  width: clamp(calc(375px - 32px), 100%, 600px);
-  height: 75%;
+  width: clamp(calc(375px - 32px), 100%, 90vh);
+  max-height: 75%;
 
   padding: 1rem;
   margin: 3rem auto; 
 
   font-size: 1.25rem; 
-  overflow-y: scroll;
+  overflow-y: auto;
 `
