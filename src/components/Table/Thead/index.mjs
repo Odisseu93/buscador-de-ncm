fdfc17eba6b4238b8ca66b55b071ad34dcf7dbef
Ulisses Silvério <odisseu93@localhost.localdomain>
2023-07-12 22:03:18 -0300

    fix: fix Thead's HTML

--- src/components/Table/Thead/index.mjs
+++ src/components/Table/Thead/index.mjs
@@ -1,11 +1,10 @@
+import { fragment } from '../../../helpers/fragment.mjs'
 import { Th } from '../Th/index.mjs'
 import { Tr } from '../Tr/index.mjs'
 import * as S from './styles.mjs'
 
-export const Thead = (children) => {
- 
-  return `
-<thead style="${S.Thead}">
-${Tr(children.map(textcontent => Th(textcontent)))}
-</thead>
-`}
+export const Thead = (children) => (`
+  <thead style="${S.Thead}">
+    ${Tr(fragment(children.map(textcontent => Th(textcontent))))}
+  </thead>
+`)
